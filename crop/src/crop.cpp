1941c73d9a01cf749aba31d2ddf470ca38b4f811
Your Name <you@example.com>
2018-07-26 17:52:19 +0700

    crop

---
+++ crop/src/crop.cpp
@@ -0,0 +1,36 @@
+#include "blob.h"
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace std;
+int main(int argc, char* argv[])
+{
+  string image_file_name = "../image/origin.png";
+  cv::Mat img = cv::imread(image_file_name);
+  if (img.empty())
+    {
+      std::cout << "!!! imread() failed to open target image" << std::endl;
+      return -1;
+    }
+
+  /* Set Region of Interest */
+
+  int offset_x = 129;
+  int offset_y = 129;
+
+  cv::Rect roi;
+  roi.x = offset_x;
+  roi.y = offset_y;
+  roi.width = img.size().width - (offset_x*2);
+  roi.height = img.size().height - (offset_y*2);
+
+  /* Crop the original image to the defined ROI */
+
+  cv::Mat crop = img(roi);
+  cv::imshow("crop", crop);
+  cv::waitKey(0);
+
+  // cv::imwrite("../image/crop.png", crop);
+
+  return 0;
+}
